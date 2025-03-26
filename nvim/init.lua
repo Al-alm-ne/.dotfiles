50407b10e51c65242e67a31c04d642231f84aafc
Cluster <185813814+Al-alm-ne@users.noreply.github.com>
2025-03-26 10:11:49 +0000

    nvim

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- Core
+-- Global options
+require("alex.core.global")
+-- Options
+require("alex.core.options")
+-- Key Mappings
+require("alex.core.mappings")
+-- Auto Commands
+require("alex.core.autocmd")
+-- Macros
+require("alex.core.macros")
+
+-- Lazy Plugin Manager
+require("alex.lazy")
